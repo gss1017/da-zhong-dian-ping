d94cf06e8aa17a3b516719aa87962baebc6b97c3
郭帅帅 <704555087@qq.com>
2019-10-28 15:43:22 +0800

    减少不必要的打包信息

--- src/pages/a.jsx
+++ src/pages/a.jsx
@@ -1,5 +1,17 @@
 import React from 'react';
+import map from 'lodash/map';
 
 export default function () {
-    return <div>这里是A</div>;
+    return (
+        <div>
+            <div>这里是A</div>
+            {map([1, 2, 3], (item) => {
+                return (
+                    <div key={item}>
+                        {item}
+                    </div>
+                );
+            })}
+        </div>
+    );
 }
